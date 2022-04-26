a38711829f9e2170b1b3ada8a4da7aeb9b0ab615
JoseMMorales <josemulamorales33@hotmail.com>
2022-04-26 12:56:36 +0200

    Including button to get back to Select page in not-found page

--- src/app/core/material.module.ts
+++ src/app/core/material.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 
-const Material = [
-  MatSelectModule,
-  MatProgressSpinnerModule
-];
+const Material = [MatSelectModule, MatProgressSpinnerModule, MatButtonModule];
 
 @NgModule({
   exports: [Material],
-  imports: [Material]
+  imports: [Material],
 })
-export class MaterialModule { }
+export class MaterialModule {}
